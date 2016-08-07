ba5fa45dc6a561fb6a9484c83c0122cb9845bc2b
Dan Corin <dancorin@uber.com>
2016-08-07 14:20:11 -0700

    Update install script.

--- install.sh
+++ install.sh
@@ -1,3 +1,37 @@
-rcup -x install.sh
-. ~/.bashrc
+#!/bin/bash
 
+
+printf 'checking for dotfiles...'
+if [ -d "$HOME/.dotfiles" ]; then
+	printf "dotfiles already downloaded\n"
+else
+	printf "downloading dotfiles..."
+	git clone git@github.com:danielcorin/dotfiles.git "$HOME/.dotfiles"
+	printf "dotfiles downloaded\n"
+fi
+
+printf "checking rcm installation..."
+which rcup > /dev/null 2>&1
+
+if [ $? -eq 1 ]; then
+	if [ "$(uname)" = "Darwin" ]; then
+		brew tap thoughtbot/formulae
+		brew install rcm
+		printf "installed rcm\n"
+	else
+		sudo add-apt-repository ppa:martin-frost/thoughtbot-rcm
+		sudo apt-get update
+		sudo apt-get install rcm
+		printf "installed rcm\n"
+	fi
+else
+	printf "rcm already installed\n"
+fi
+
+printf "installing dotfiles..."
+rcup -x install.sh
+printf "dotfiles installed\n"
+
+printf "sourcing bashrc..."
+. ~/.bashrc
+printf "bashrc sourced\n"
