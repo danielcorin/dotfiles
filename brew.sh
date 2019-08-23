8b754ae0a3fc413daca03fc2eb98fe306b5777d2
Dan Corin <dancorin@uber.com>
2019-08-23 12:58:45 -0700

    Git and brew stuff

--- brew.sh
+++ brew.sh
@@ -3,17 +3,19 @@ brew upgrade
 
 brew install ack
 brew install bat
+brew install ccze
 brew install coreutils
 brew install exa
 brew install fd
 brew install fzf
+brew install gron
 brew install lua
+brew install md5sha1sum
 brew install ossp-uuid
 brew install ssh-copy-id
+brew install telnet
 brew install tree
 brew install yank
-brew install ccze
-brew install telnet
-brew install md5sha1sum
+brew install protoc-gen-go
 
 brew cleanup
